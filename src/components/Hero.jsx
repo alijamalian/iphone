76679dcf176eeb9c98f880f202cc4540ed681780
alijamalian <alijamalian@gmail.com>
2025-04-01 00:25:29 +0200

    hero-animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { heroVideo, smallHeroVideo } from '../utils'
+
+import { useState } from 'react'
 
 const Hero = () => {
-	return <div>Hero</div>
+	const [videoSrc, setVideoSrc] = useState(
+		window.innerWidth < 760 ? smallHeroVideo : heroVideo
+	)
+	useGSAP(() => {
+		gsap.to('#hero', { opacity: 1, delay: 1.5 })
+	}, [])
+	return (
+		<section className="w-full nav-height bg-black relative">
+			<div className="h-5/6 w-full flex-center flex-col">
+				<p id="hero" className="hero-title">
+					Iphone 15 Pro
+				</p>
+				<div className="md">
+					<video>
+						<source src={videoSrc} />
+					</video>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Hero
