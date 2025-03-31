5afab09f714154a3a9eba4842fba81ca59260fe6
alijamalian <alijamalian@gmail.com>
2025-03-31 07:10:28 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import Highlights from './components/Highlights';
+// import Model from './components/Model';
+// import Features from './components/Features';
+// import HowItWorks from './components/HowItWorks';
+// import Footer from './components/Footer';
 
-import * as Sentry from '@sentry/react';
+// import * as Sentry from '@sentry/react';
 
 const App = () => {
-  return (
-    <main className="bg-black">
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
-    </main>
-  )
+	return <h1 className="text-3xl font-bold underline">Hello World!</h1>
 }
 
-export default Sentry.withProfiler(App);
+// export default Sentry.withProfiler(App);
+export default App
